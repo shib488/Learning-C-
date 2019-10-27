177340d77e9e161465d0ebbc7aa395b3720ccca0
shib488 <57055662+shib488@users.noreply.github.com>
2019-10-27 11:14:56 +0530

    Add files via upload

---
+++ InsertionSort.cpp
@@ -0,0 +1,32 @@
+/**
+ * @Author: Rishav Kumar <rishavk773>
+ * @Email: rishavk773@gmail.com
+ */
+
+ /* Time Complexity:-
+  *       Best case = n
+  *       Average case = n^2
+  *       worst case = n^2
+  *
+  * INPUT : 12  11  31  4  23  -5  99  31  11
+  * OUTPUT : Initial array elements : 12  11  31  4  23  -5  99  31  11
+  *          After sorting array elements : -5  4  11  11  12  23  31  31  99
+  */
+
+#include <iostream>
+#include <stdlib.h>
+#include "header.h"
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+    int i,ar[argc-1];
+    for(i=1;i<argc;i++)
+        ar[i-1]=atoi(argv[i]);
+    cout<<"Initial array elements : ";
+    traverse(ar,argc-1);
+    insertionSort(ar,argc-1);
+    cout<<"After sorting array elements : ";
+    traverse(ar,argc-1);
+    return 0;
+}
